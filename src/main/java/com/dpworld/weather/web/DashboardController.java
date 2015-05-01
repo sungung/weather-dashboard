9ac30da8d0e0d6917034034abdfc60f6002a8072
sungung <sungung.park@gmail.com>
2015-05-01 16:37:54 +1000

    Add angularjs feature

---
+++ src/main/java/com/dpworld/weather/web/DashboardController.java
@@ -0,0 +1,25 @@
+package com.dpworld.weather.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author PARK Sungung
+ * @since 0.0.1
+ */
+@RestController
+public class DashboardController {
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+}
